6721c67bfc899797112bf9a7253ce9ef7cb978f4
AdityaAshvin <53205379+AdityaAshvin@users.noreply.github.com>
2019-08-11 17:39:46 +0530

    Update codeforces282A.py

--- task 2/codeforces/codeforces282A.py
+++ task 2/codeforces/codeforces282A.py
@@ -1,10 +1,11 @@
-n = int(input(" enter a number "))
+n = int(input())
 x = 0
 for test in range(n):
-    test = input(" enter ++x for incresing the value or --x for decreasing the value ")
+    test = input( )
     if (test[1] == "+"):
         x = x+1
     else:
         x = x-1
 
 print(x)
+
