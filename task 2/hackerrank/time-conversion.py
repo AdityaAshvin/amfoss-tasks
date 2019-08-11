8582683a20a917631f14262f445c85fef06405c6
AdityaAshvin <53205379+AdityaAshvin@users.noreply.github.com>
2019-08-11 21:06:30 +0530

    Update time-conversion.py

--- task 2/hackerrank/time-conversion.py
+++ task 2/hackerrank/time-conversion.py
@@ -1,12 +1,17 @@
-string1 = input("enter the time in 12 hour AM?PM format")
+string1 = input()
 if (string1[-2:] == "AM" and string1[ :2] == "12"):
-    print( "00" + string1[2:-2])   #checking if the last two digits of the string is AM or PM and changing 12 with 00
-elif string1[-2: ] == "AM":
-    print(string1[ :-2])          #dropping AM
-elif (string1[ :-2] == "PM" and string1[ :2] == "12"):
-    print(string1[ :-2])         #dropping PM
+    print( "00" + string1[2:-2])         #checking if the last two digits of the string is AM or PM and changing 12 with 00
+elif (string1[-2: ] == "AM"):
+    print(string1[ :-2])                 #dropping AM
+elif (string1[-2:] == "PM" and string1[ :2] == "12"):
+    print( "12" + string1[2:-2])
 else:
-    print(str(int(string1[:2]) + 12) + string1[2:8]) #printing in 24 hour format.
+    print(str(int(string1[:2]) + 12) + string1[2:8])      #printing in 24 hour format.
+
+    
+
+    
+
 
     
 
