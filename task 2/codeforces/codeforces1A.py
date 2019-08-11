aeb91c6e0b1ba6d073fec5751f6f080a14e7e22b
AdityaAshvin <53205379+AdityaAshvin@users.noreply.github.com>
2019-08-11 17:11:03 +0530

    Update codeforces1A.py

--- task 2/codeforces/codeforces1A.py
+++ task 2/codeforces/codeforces1A.py
@@ -1,13 +1,13 @@
 n,m,a = map(int,input( ).split())
 if(n%a == 0):      #rowwise
-    b = (n/a)
+    b = (n//a)
 else:
     b = ((n//a)+1)
-
+ 
 if(m%a == 0):      #columnwise
-    c = (m/a)
+    c = (m//a)
 else:
     c = ((m//a)+1)
-
+ 
 print(b*c)     #printing number of flagstones req
 
