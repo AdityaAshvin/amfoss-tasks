0f1e0691c1e021c827177b5075ce374ad8484169
AdityaAshvin <53205379+AdityaAshvin@users.noreply.github.com>
2019-08-11 17:09:43 +0530

    Update codeforces1A.py

--- task 2/codeforces/codeforces1A.py
+++ task 2/codeforces/codeforces1A.py
@@ -1,12 +1,13 @@
-n,m,a = map(int,input( "enter n, m and a where n*m is the size of the rectangular field and a*a is the size of the flagstones ").split())
+n,m,a = map(int,input( ).split())
 if(n%a == 0):      #rowwise
-    b = (n//a)
+    b = (n/a)
 else:
-    b = (n//(a+1))
+    b = ((n//a)+1)
 
 if(m%a == 0):      #columnwise
-    c = (m//a)
+    c = (m/a)
 else:
-    c = (m//(a+1))
+    c = ((m//a)+1)
 
 print(b*c)     #printing number of flagstones req
+
