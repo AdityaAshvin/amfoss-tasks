5cdecde50d52d4c996d7b6c8bb3263e8286b7098
AdityaAshvin <53205379+AdityaAshvin@users.noreply.github.com>
2019-09-17 16:43:21 +0530

    Update twitter.go

--- task 6/twitter.go
+++ task 6/twitter.go
@@ -1,20 +1,38 @@
 package main
 
-import ("encoding/json"
-	"github.com/ChimeraCoder/anaconda"
-	"flag"
-	"os")
-        
+import (
+    "os"
+    "flag"
+    "fmt"
+    "github.com/dghubble/go-twitter/twitter"
+    "github.com/dghubble/oauth1"
+	
+)
 
 func main() {
-	handlePtr := flag.String("handle", "TwitterAPI", "Twitter handle")
-	anaconda.SetConsumerKey("u1IfrGHbISQOEahLSu1uNHX6U")
-	anaconda.SetConsumerSecret("1B2pfbKwwQUo1XJDENy8dXFxtu7e9dkW8MQTSxixNp2epz1C5o")
-	api_client := anaconda.NewTwitterApi("1171113971119575040-j9yYMQgautaDZE3vDIlgQ6x7gYf34N ", "SYeDwgctvOOORHFJCbu8eo5a00l0kRIsu9EpDSMWamLGj")
-	var user anaconda.User
-	user, _ = api_client.GetUsersShow(*handlePtr, nil)
-	userJSON, _ := json.MarshalIndent(user, "", " ")
-	f, _ := os.Create("./output.txt")
-	f.WriteString(string(userJSON))
-	f.Sync()
-	f.Close()
+	var name string    
+	fmt.Println("enter the username ")
+	fmt.Scanln(&name)
+	tn:=flag.String("twitterHandler",name,"it contains the name of twitter handler") 
+	flag.Parse()
+	config := oauth1.NewConfig("u1IfrGHbISQOEahLSu1uNHX6U", "1B2pfbKwwQUo1XJDENy8dXFxtu7e9dkW8MQTSxixNp2epz1C5o")
+	token := oauth1.NewToken("1171113971119575040-j9yYMQgautaDZE3vDIlgQ6x7gYf34N ", "SYeDwgctvOOORHFJCbu8eo5a00l0kRIsu9EpDSMWamLGj")
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+	f, err := os.Create("userHandler.txt")
+
+	params := &twitter.FollowerListParams{
+	        ScreenName: *tn,
+	}
+    
+	followers, resp, err := client.Followers.List(params)
+	var count int = 0;
+	fmt.Println(resp, err)
+	f.WriteString("Followers - " + *tn)
+	for _, follower := range followers.Users {
+		count=count+1
+		f.WriteString("\n" + follower.ScreenName)
+	}
+	f.WriteString("\n")
+	f.WriteString(fmt.Sprintf("\n", count))
+    f.Close()}
